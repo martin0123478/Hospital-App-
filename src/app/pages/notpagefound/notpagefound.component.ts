2596aff9c3c37d81af1836a5ee3df4c741c7f3c4
martin0123478 <martin_agui97@outlook.es>
2022-01-04 21:26:23 -0600

    creacion pagina 404

--- src/app/pages/notpagefound/notpagefound.component.ts
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notpagefound',
   templateUrl: './notpagefound.component.html',
-  styles: [
-  ]
+  styleUrls: ['./notpagefound.component.css']
 })
 export class NotpagefoundComponent implements OnInit {
 
+  year = new Date().getFullYear()
   constructor() { }
 
   ngOnInit(): void {
