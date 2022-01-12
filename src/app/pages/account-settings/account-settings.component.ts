160189e53d097f84e1f8c6e12b0758316528bbe6
martin0123478 <martin_agui97@outlook.es>
2022-01-11 22:56:36 -0600

    cambio de color de forma dinamica

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-settings.component.css']
 })
 export class AccountSettingsComponent implements OnInit {
-
+  public linkTheme = document.querySelector('#theme');
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeTheme(theme:string){
+   
+   const url = `./assets/css/colors/${theme}.css`
+   this.linkTheme?.setAttribute('href',url)
+   localStorage.setItem('theme',url)
   }
 
 }
