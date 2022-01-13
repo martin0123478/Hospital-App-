3aaf84d41c0305aa7dd7d8e8a7605a9dc857a371
martin0123478 <martin_agui97@outlook.es>
2022-01-12 20:57:02 -0600

    se pone  check al color seleccionado

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -13,10 +13,24 @@ export class AccountSettingsComponent implements OnInit {
   }
 
   changeTheme(theme:string){
-   
    const url = `./assets/css/colors/${theme}.css`
    this.linkTheme?.setAttribute('href',url)
    localStorage.setItem('theme',url)
+   this.checkAccount()
+  }
+
+  checkAccount(){
+    const links = document.querySelectorAll('.selector')
+    links.forEach(element=>{
+      element.classList.remove('working');
+      const btnTheme = element.getAttribute('data-theme')
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`
+      const currentTheme = this.linkTheme?.getAttribute('href')
+      if(btnThemeUrl === currentTheme){
+        element.classList.add('working')
+      }
+
+    })
   }
 
 }
