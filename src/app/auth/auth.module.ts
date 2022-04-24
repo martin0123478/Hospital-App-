5c385a8d2db3a38527aa2a8c9d88e05b6d58bf59
martin0123478 <martin_agui97@outlook.es>
2022-04-24 11:20:30 -0500

    creando el fomularo reactivo para el registro de usuarios

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import {RouterModule} from '@angular/router'
-import {FormsModule} from '@angular/forms'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -13,7 +13,8 @@ import {FormsModule} from '@angular/forms'
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports:[
     LoginComponent,
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  constructor(private fb:FormBuilder) { }
+public registerForm= this.fb.group({
+  nombre:['martin',[Validators.required,Validators.minLength(3)]],
+  email:['test100@gmail.com',Validators.required],
+  password:['123456', Validators.required],
+  password2:['123456', Validators.required],
+  terminos:[false, Validators.required],
 
-  constructor() { }
+});
+
+  crearUsuario(){
+    console.log(this.registerForm.value)
+  }
+  
 
   ngOnInit(): void {
   }
